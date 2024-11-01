b0ec7e34777a15df75dd09eed288edcc0e916fb4
Pablo <info@pabloperez.com.es>
2024-11-01 09:56:15 +0100

    last tests and jest conf

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -48,36 +48,38 @@ describe('Test Transactions', () => {
     })
 
     test('Create transaction', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX1" }).expect(200);
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX" }).expect(200);
         expect(res.body._id).toBeDefined()
-        expect(res.body.name).toBe('TRX1')
+        expect(res.body.name).toBe('TRX')
         expect(res.body.status).toBe(TransactionStatus.Pending)
         const trx = res.body;
         res = await request(server).patch(`${basePath}/${trx._id}/validation`).set("Authorization", `Bearer ${adminToken}`).send({ "status": TransactionStatus.Approved }).expect(200);
         expect(res.body.status).toBe(TransactionStatus.Approved)
+        res = await request(server).patch(`${basePath}/${trx._id}/approval`).set("Authorization", `Bearer ${userToken}`).send().expect(200);
+        expect(res.body.status).toBe(TransactionStatus.Done)
     })
 
     test('Create transaction and reject it', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX2" }).expect(200);
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX" }).expect(200);
         const trx = res.body;
         res = await request(server).patch(`${basePath}/${trx._id}/validation`).set("Authorization", `Bearer ${adminToken}`).send({ "status": TransactionStatus.Rejected }).expect(200);
         expect(res.body.status).toBe(TransactionStatus.Rejected)
     })
 
     test('Create transaction without auth', async () => {
-        const res = await request(server).post(`${basePath}/`).send({ "name": "TRX4" }).expect(401);
+        const res = await request(server).post(`${basePath}/`).send({ "name": "TRX" }).expect(401);
         expect(res.body.code).toBe('Unauthorized')
     })
 
     test('Try to create an approved transaction', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX2", "status": TransactionStatus.Approved }).expect(200);
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX", "status": TransactionStatus.Approved }).expect(200);
         expect(res.body._id).toBeDefined()
-        expect(res.body.name).toBe('TRX2')
+        expect(res.body.name).toBe('TRX')
         expect(res.body.status).toBe(TransactionStatus.Pending)
     })
 
     test('Try to create and approve transaction without auth', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX3" }).expect(200);
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX" }).expect(200);
         const trx = res.body;
         // Invocamos la validacion sin token debe fallar
         res = await request(server).patch(`${basePath}/${trx._id}/validation`).send({ "status": TransactionStatus.Approved }).expect(401);
@@ -85,20 +87,26 @@ describe('Test Transactions', () => {
     })
 
     test('Try to create and self approve transaction', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX3" }).expect(200);
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX" }).expect(200);
         const trx = res.body;
         // Invocamos la validacion con token de usuario y debe fallar
         res = await request(server).patch(`${basePath}/${trx._id}/validation`).set("Authorization", `Bearer ${userToken}`).send({ "status": TransactionStatus.Approved }).expect(403);
         expect(res.body.code).toBe('Forbidden')
     })
 
-    test('Create transaction and approve with invalid value', async () => {
-        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX1" }).expect(200);
+    test('Try to create transaction and approve with invalid value', async () => {
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${userToken}`).send({ "name": "TRX" }).expect(200);
         expect(res.body._id).toBeDefined()
-        expect(res.body.name).toBe('TRX1')
+        expect(res.body.name).toBe('TRX')
         expect(res.body.status).toBe(TransactionStatus.Pending)
         const trx = res.body;
         res = await request(server).patch(`${basePath}/${trx._id}/validation`).set("Authorization", `Bearer ${adminToken}`).send({ "status": "foo" }).expect(422);
         expect(res.body.code).toBe('InvalidTransactionStatus')
+    })
+
+    test('Try to create transaction with fake JWT token', async () => {
+        const fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAxSkJIMERBOFBGQUdBQ0ZKVDE4NEcyRk02Iiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MzA0NDk3MDIsImV4cCI6MTczMDUzNjEwMn0.jYqZG1wFrrbe4BILvAHZt05aynxaa_6r1Us5sD4XQx8';
+        res = await request(server).post(`${basePath}/`).set("Authorization", `Bearer ${fakeToken}`).send({ "name": "TRX" }).expect(401);
+        expect(res.body.code).toBe('Unauthorized')
     })
 })
